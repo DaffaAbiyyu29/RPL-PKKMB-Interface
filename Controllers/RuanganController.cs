1b61860af3387f024c7e73025034679fb320ea83
Muhammad Daffa Abiyyu Syaddad <daffaabiyyu04@gmail.com>
2023-12-22 22:16:25 +0700

    pengumpulan

--- Controllers/RuanganController.cs
+++ Controllers/RuanganController.cs
@@ -8,5 +8,17 @@ namespace PKKMB_Interface.Controllers
 		{
 			return View();
 		}
+
+		public IActionResult tambahruangan()
+		{
+			return View();
+		}
+
+		[Route("ruangan/ubahruangan/{rng_idruangan}")]
+		public IActionResult ubahruangan(string rng_idruangan)
+		{
+			ViewBag.rng_idruangan = rng_idruangan;
+			return View();
+		}
 	}
 }
