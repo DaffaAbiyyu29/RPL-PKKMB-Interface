1b02ddd8a38b07107470a6bacf2e986991e09ffb
ziban321 <zibanrobani@gmail.com>
2023-12-25 16:45:24 +0700

    ziban cru dan interface absensi

--- Controllers/AbsensiController.cs
+++ Controllers/AbsensiController.cs
@@ -2,11 +2,18 @@
 
 namespace PKKMB_Interface.Controllers
 {
-	public class AbsensiController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class AbsensiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Absensi/Create/{abs_idabsensi}")]
+        public IActionResult Create(string abs_idabsensi)
+        {
+            ViewBag.abs_idabsensi = abs_idabsensi;
+            return View();
+        }
+    }
 }
