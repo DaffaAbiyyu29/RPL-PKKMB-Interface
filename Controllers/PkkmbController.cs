502e1958c9be3a0b02a9deaa04bc73bc4f042a4f
Nabilabidah <abidahhh21@gmail.com>
2024-01-16 15:48:14 +0700

    pkkmb filter

---
+++ Controllers/PkkmbController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PKKMB_Interface.Controllers
+{
+	public class PkkmbController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+		public IActionResult TambahTahun()
+		{
+			return View();
+		}
+
+		[Route("/Pkkmb/UbahTahun/{pkm_idPkkmb}")]
+		public IActionResult UbahTahun(string pkm_idPkkmb)
+		{
+			ViewBag.pkm_idPkkmb = pkm_idPkkmb;
+			return View();
+		}
+	}
+}
