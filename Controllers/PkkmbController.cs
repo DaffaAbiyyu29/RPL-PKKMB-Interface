f9996387c570058b35fc0ba68fc07ecebeda39cd
Muhammad Daffa Abiyyu Syaddad <daffaabiyyu04@gmail.com>
2024-01-18 17:53:06 +0700

    filter tahun pkkmb

--- Controllers/PkkmbController.cs
+++ Controllers/PkkmbController.cs
@@ -1,24 +1,108 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PKKMB_Interface.Models;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace PKKMB_Interface.Controllers
 {
 	public class PkkmbController : Controller
 	{
+		private readonly ValidateToken _valid;
+
+		public PkkmbController(ValidateToken validateToken)
+		{
+			_valid = validateToken;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			if (HttpContext.Request.Cookies["token"] != null)
+			{
+				string userToken = HttpContext.Request.Cookies["token"];
+
+				if (_valid.ValidateTokenFromCookies(userToken))
+				{
+					ViewBag.UserToken = userToken;
+
+					var handler = new JwtSecurityTokenHandler();
+					var jsonToken = handler.ReadToken(userToken) as JwtSecurityToken;
+					var userId = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "id")?.Value;
+					var userName = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "name")?.Value;
+					var userRole = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "role")?.Value;
+
+					if (userRole == "PIC PKKMB")
+					{
+						ViewBag.UserId = userId;
+						ViewBag.UserName = userName;
+						ViewBag.UserRole = userRole;
+
+						return View();
+					}
+				}
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
-		
+
 		public IActionResult TambahTahun()
 		{
-			return View();
+			if (HttpContext.Request.Cookies["token"] != null)
+			{
+				string userToken = HttpContext.Request.Cookies["token"];
+
+				if (_valid.ValidateTokenFromCookies(userToken))
+				{
+					ViewBag.UserToken = userToken;
+
+					var handler = new JwtSecurityTokenHandler();
+					var jsonToken = handler.ReadToken(userToken) as JwtSecurityToken;
+					var userId = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "id")?.Value;
+					var userName = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "name")?.Value;
+					var userRole = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "role")?.Value;
+
+					if (userRole == "PIC PKKMB")
+					{
+						ViewBag.UserId = userId;
+						ViewBag.UserName = userName;
+						ViewBag.UserRole = userRole;
+
+						return View();
+					}
+				}
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 
 		[Route("/Pkkmb/UbahTahun/{pkm_idPkkmb}")]
 		public IActionResult UbahTahun(string pkm_idPkkmb)
 		{
-			ViewBag.pkm_idPkkmb = pkm_idPkkmb;
-			return View();
+			if (HttpContext.Request.Cookies["token"] != null)
+			{
+				string userToken = HttpContext.Request.Cookies["token"];
+
+				if (_valid.ValidateTokenFromCookies(userToken))
+				{
+					ViewBag.UserToken = userToken;
+
+					var handler = new JwtSecurityTokenHandler();
+					var jsonToken = handler.ReadToken(userToken) as JwtSecurityToken;
+					var userId = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "id")?.Value;
+					var userName = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "name")?.Value;
+					var userRole = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "role")?.Value;
+
+					if (userRole == "PIC PKKMB")
+					{
+						ViewBag.UserId = userId;
+						ViewBag.UserName = userName;
+						ViewBag.UserRole = userRole;
+						ViewBag.pkm_idPkkmb = pkm_idPkkmb;
+
+						return View();
+					}
+				}
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
