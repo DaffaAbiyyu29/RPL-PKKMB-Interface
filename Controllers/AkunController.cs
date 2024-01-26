33927c8a85ec4709e16267751f80043b435389f5
Muhammad Daffa Abiyyu Syaddad <daffaabiyyu04@gmail.com>
2024-01-26 14:08:39 +0700

    reset password

--- Controllers/AkunController.cs
+++ Controllers/AkunController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PKKMB_Interface.Models;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace PKKMB_Interface.Controllers
 {
 	public class AkunController : Controller
 	{
+		private readonly ValidateToken _valid;
+
+		public AkunController(ValidateToken validateToken)
+		{
+			_valid = validateToken;
+		}
+
 		[Route("/Akun/Login")]
 		public IActionResult Login()
 		{
@@ -18,6 +27,33 @@ namespace PKKMB_Interface.Controllers
 		public IActionResult LupaKataSandi()
 		{
 			return View();
+		}
+
+		public IActionResult ResetKataSandi()
+		{
+			if (HttpContext.Request.Cookies["VerifyEmail"] != null)
+			{
+				string userToken = HttpContext.Request.Cookies["VerifyEmail"];
+
+				if (_valid.ValidateTokenFromCookies(userToken))
+				{
+					ViewBag.UserToken = userToken;
+
+					var handler = new JwtSecurityTokenHandler();
+					var jsonToken = handler.ReadToken(userToken) as JwtSecurityToken;
+					var userId = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "id")?.Value;
+					var userName = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "name")?.Value;
+					var userRole = jsonToken?.Claims?.FirstOrDefault(claim => claim.Type == "role")?.Value;
+
+					ViewBag.UserId = userId;
+					ViewBag.UserName = userName;
+					ViewBag.UserRole = userRole;
+
+					return View();
+				}
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 
 		[Route("/Akun/Ubah/{username}")]
